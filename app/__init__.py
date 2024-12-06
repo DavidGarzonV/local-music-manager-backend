f93ac94cd55b7f858a267bfe46a6a3be91611b76
David Garzón Vélez <32543229+DavidGarzonV@users.noreply.github.com>
2024-12-06 14:43:05 -0500

    feat: Add DELETE to CORS

--- app/__init__.py
+++ app/__init__.py
@@ -50,7 +50,7 @@ def create_app():
         resources={
             r"/api/*": {
                 "origins": ENABLED_ORIGIN,
-                "methods": ["GET", "POST", "OPTIONS", "HEAD"],
+                "methods": ["GET", "POST", "OPTIONS", "HEAD", "DELETE"],
                 "allow_headers": ["Authorization, Content-Type"],
             }
         },
